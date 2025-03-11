ad3bd47f03a1f594c3141e856c4a933eba95a8a5
Stefan.Seeland <168659+stesee@users.noreply.github.com>
2025-03-11 11:23:06 +0100

    Initial commit

---
+++ NetMauiOptainiumHelloWorld/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NetMauiOptainiumHelloWorld
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NetMauiOptainiumHelloWorld/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NetMauiOptainiumHelloWorld
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
